9ec2299da44653c1b7a9cefbbfaafd79f92eedcf
shimmh <31092342+shimmh@users.noreply.github.com>
2021-07-24 15:10:57 +0900

    Create Home Screen

---
+++ src/Routes/Home/HomePresenter.js
@@ -0,0 +1,82 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "Components/Section";
+import Loader from "Components/Loader";
+import Message from "Components/Message";
+import Poster from "Components/Poster";
+import Helmet from "react-helmet";
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) => (
+  <>
+    <Helmet>
+      <title>Movie | Nomfilx</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {nowPlaying && nowPlaying.length > 0 && (
+          <Section title="Now Playing">
+            {nowPlaying.map((movie) => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {upcoming && upcoming.length > 0 && (
+          <Section title="upcoming Movies">
+            {upcoming.map((movie) => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Movies">
+            {popular.map((movie) => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e74c3c" text={error} />}
+      </Container>
+    )}
+    ;
+  </>
+);
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default HomePresenter;
