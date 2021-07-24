4787ecc19c95ccf346fddda13c4aed05be3ca220
shimmh <31092342+shimmh@users.noreply.github.com>
2021-07-24 15:22:59 +0900

    moviedb api 변경(http-> https)

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.themoviedb.org/3/",
+  baseURL: "https://api.themoviedb.org/3/",
   params: {
     api_key: "4c834bc8a1eb1cda919768461786d872",
     language: "en-US",
