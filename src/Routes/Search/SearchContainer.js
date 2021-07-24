ec2d2de3e9af36c982dbd510f3df323d4fb70005
shimmh <31092342+shimmh@users.noreply.github.com>
2021-07-24 15:11:28 +0900

    Create Search Screen

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,71 @@
+import { moviesApi, tvApi } from "api";
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+
+class SearchContainer extends React.Component {
+  state = {
+    movieResults: null,
+    tvResults: null,
+    searchTerm: "",
+    loading: false,
+    error: null,
+  };
+
+  // componentDidMount() {
+  //   this.handleSubmit();
+  // }
+
+  handleSubmit = (event) => {
+    event.preventDefault(); //submit 막기 위해
+
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  updateTerm = (event) => {
+    const {
+      target: { value },
+    } = event;
+    // console.log(value);
+    this.setState({ searchTerm: value });
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+
+      this.setState({ movieResults, tvResults });
+    } catch {
+      this.setState({ error: "Can't find results" });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+    // console.log(this.state);
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+        handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
+      />
+    );
+  }
+}
+
+export default SearchContainer;
